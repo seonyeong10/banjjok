99f301e77281a8fe65c280b1f7387d3c269469ec
kjs63 <kjs63@192.168.123.107>
2021-03-12 15:46:39 +0900

    css와 img 나옴

--- banjjok/src/main/java/banjjok/contoller/main/MainController.java
+++ banjjok/src/main/java/banjjok/contoller/main/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 	@RequestMapping(value = "/")
 	public String home() {
-		return "thymeleaf/main";
+		return "main";
 	}
 }
