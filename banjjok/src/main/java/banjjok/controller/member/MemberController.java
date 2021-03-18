475874bedffe299a43ab710cd7a2f7e1f3607673
ezen <ezen@DESKTOP-O03MT84>
2021-03-18 19:10:15 +0900

    memberController 수정

--- banjjok/src/main/java/banjjok/controller/member/MemberController.java
+++ banjjok/src/main/java/banjjok/controller/member/MemberController.java
@@ -1,9 +1,41 @@
 package banjjok.controller.member;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import banjjok.command.MemberCommand;
+import banjjok.service.mem.ChkPassService;
+import banjjok.service.mem.ModifyService;
 
 @Controller
-@RequestMapping("register")
+@RequestMapping("main")
 public class MemberController {
-}
+	@Autowired
+	ChkPassService chkPassService;
+	@Autowired
+	ModifyService modifyService;
+	@RequestMapping(value = "myPage", method = RequestMethod.GET)
+	public String myPage() {
+		return "member/memPage";
+	}
+	@RequestMapping(value = "modifyMem", method = RequestMethod.GET)
+	public String modifyMem() {
+		return "member/checkPw";
+	}
+	@RequestMapping(value = "goModify", method = RequestMethod.POST)
+	public String goModify(@RequestParam(value = "memPw") String memPw, Model model, HttpSession session) throws Exception {
+		String path = chkPassService.check(memPw, model, session);
+		return path;
+	}
+	@RequestMapping(value = "modifyAct", method = RequestMethod.POST)
+	public String modifyAct(MemberCommand memberCommand, Model model, HttpSession session) throws Exception {
+//		String path = chkPassService.check(memberCommand, model, session);
+		return "";
+	}
+} 
